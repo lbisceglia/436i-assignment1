d8926a3c0f792a1e02b5c87ca0bba85579d3d472
Lorenzo Bisceglia <lorenzo.bisceglia9@gmail.com>
2019-05-15 14:05:32 -0700

    implemented add and clear functionality to messages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,40 @@
-alert("Hello World!");
+const DEFAULT_MSG = {
+  "bulletins" : [
+    {"message": "Check the 436I Slack channel"},
+    {"message": "Finish updating my resume"},
+    {"message": "Watch the finale of Game of Thrones"}
+  ]
+}
+
+let bulletins = JSON.parse(DEFAULT_MSG);
+
+function clearMessageBox() {
+  document.getElementById("msg-box").value = "";
+}
+
+// Create a new list item when clicking on the "Add" button
+function postBulletin() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("msg-box").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("bulletins").appendChild(li);
+  }
+  clearMessageBox();
+
+  // var span = document.createElement("SPAN");
+  // var txt = document.createTextNode("\u00D7");
+  // span.className = "close";
+  // span.appendChild(txt);
+  // li.appendChild(span);
+  //
+  // for (i = 0; i < close.length; i++) {
+  //   close[i].onclick = function() {
+  //     var div = this.parentElement;
+  //     div.style.display = "none";
+    }
+  }
+}
